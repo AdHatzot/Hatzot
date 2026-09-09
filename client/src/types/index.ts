eff73532e6687fae460024031d90ad64b5fe722e
Ofir Rivitz <ofirivitz@gmail.com>
2026-09-09 10:37:07 +0300

    feat(): Add ReloadTime and Range records

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -24,7 +24,6 @@ export type Launcher = {
   id: number;
   location: Location;
   type: LauncherType;
-  interceptor: InterceptorType;
   amount: number;
   active: boolean;
   ammunition: [InterceptorType, number][];
@@ -37,6 +36,13 @@ export enum LauncherType {
   CloudFenceArea,
 }
 
+export const ReloadTime: Record<LauncherType, number> = {
+  [LauncherType.ShieldNestLite]: 0.7,
+  [LauncherType.IronHookSR]: 1,
+  [LauncherType.HorizonEyeMX]: 0.6,
+  [LauncherType.CloudFenceArea]: 0.3,
+};
+
 export enum InterceptorType {
   BuzzStop15,
   NetWing30,
@@ -47,3 +53,14 @@ export enum InterceptorType {
   SwarmMist5,
   MicroNetR,
 }
+
+export const InterceptorRange: Record<InterceptorType, number> = {
+  [InterceptorType.BuzzStop15]: 10000,
+  [InterceptorType.NetWing30]: 10000,
+  [InterceptorType.DartFoxS]: 30000,
+  [InterceptorType.SpearMini70]: 30000,
+  [InterceptorType.SkyLanceM]: 50000,
+  [InterceptorType.FalconClipH]: 70000,
+  [InterceptorType.SwarmMist5]: 5000,
+  [InterceptorType.MicroNetR]: 7000,
+};
