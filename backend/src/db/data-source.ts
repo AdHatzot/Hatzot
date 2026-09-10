b6e8c97378ef935938f60438e5e25c090689ee88
Dean Kashi <u9760457@bsmch.net>
2026-09-10 10:05:12 +0300

    change port

--- backend/src/db/data-source.ts
+++ backend/src/db/data-source.ts
@@ -32,7 +32,7 @@ import config from "../config";
 //add to .env
 // PORT=3000
 // DB_HOST=localhost
-// DB_PORT=5433
+// DB_PORT=5432
 // DB_NAME=postgres
 // DB_USERNAME=postgres
 // DB_PASSWORD=postgres
