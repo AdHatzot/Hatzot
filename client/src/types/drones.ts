34424858a682cec4e29df6b5b5c5e0fb03ffac3b
Omer Hirshoren <u9863558@bsmch.net>
2026-09-10 09:42:54 +0300

    refactor(AlertsPanel.tsx): update drone data type

--- client/src/types/drones.ts
+++ client/src/types/drones.ts
@@ -1,7 +1,7 @@
-interface Drone {
-  type:string,
-  id:string,
-  timestamp:string,
-  heading:number,
-  launch_point:{latitude:number,longitude:number}}
-
+export interface Drone {
+  type: string;
+  id: string;
+  timestamp: string;
+  heading: number;
+  launch_point: { latitude: number; longitude: number };
+}
